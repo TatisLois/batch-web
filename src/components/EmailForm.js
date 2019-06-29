aaf2e2d73f3d27106a5693ab4dce0efc20e6ea51
Lois Tatis <ltatis1992@gmail.com>
2019-06-29 04:57:47 -0400

    updates

--- src/components/EmailForm.js
+++ src/components/EmailForm.js
@@ -1,5 +1,11 @@
 import React, { Component } from 'react';
 
+const encode = (data) => {
+  return Object.keys(data)
+      .map(key => encodeURIComponent(key) + "=" + encodeURIComponent(data[key]))
+      .join("&");
+}
+
 export class EmailForm extends Component {
   constructor() {
     super();
@@ -10,6 +16,18 @@ export class EmailForm extends Component {
   onSubmit(e) {
     e.preventDefault();
     e.stopPropagation();
+    const email = e.target.children.email.value;
+
+    fetch("/", {
+      method: "POST",
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: encode({ "form-name": "batch-web-emails", email })
+    }).catch(error => alert(error));
+
+    e.preventDefault();
+
+
+
     this.setState({ message: 'Thank you!' });
     setTimeout(() => {
       this.setState({ message: '' });
@@ -21,16 +39,18 @@ export class EmailForm extends Component {
     return (
       <form
         id="signup-form"
-        onSubmit={this.onSubmit}
+        onSubmit={this.onSubmit} // edit for form works
         method="post"
         name="batch-web-emails"
         data-netlify="true">
+        <input type="hidden" name="form-name" value="batch-web-emails" />
         <input
           type="email"
           name="email"
           id="email"
           placeholder="Email Address"
         />
+        <br/>
         <input type="submit" value="Get The App First" />
         <span className={`${message ? 'visible success' : ''} message`}>
           {message}
