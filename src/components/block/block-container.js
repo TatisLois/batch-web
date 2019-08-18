ba5a701e5891e4d787e525c6f5c956297a5608c3
Lois Tatis <ltatis1992@gmail.com>
2019-08-17 23:26:47 -0400

    refactors

--- src/components/block/block-container.js
+++ src/components/block/block-container.js
@@ -1,21 +1,25 @@
 import React from 'react';
 import Title from './title'
 import copy from './data';
+const blockImage =  require('../../assets/images/Numbers.png');
 
-export default function UpsellBlock() {
+export default function Blocks() {
   return (
     <div>
       <Title />
-      <div className='upsell'>
-        {
-            copy.map(({ title, body, anchor }) => (
-                <div className="upsell-section-body">
-                    <h1 className='upsell-title'>{ title }</h1>
-                    <p className='upsell-text'>{ body }</p>
-                    <a className='upsell-anchor-text' href="">{ anchor }</a>
-                </div>
-            ))
-        }
+      <div>
+        <img src={blockImage} className='blockImage'  />
+        <div className='upsell'>
+          {
+              copy.map(({ title, body, anchor }) => (
+                  <div className="upsell-section-body">
+                      <h1 className='upsell-title'>{ title }</h1>
+                      <p className='upsell-text'>{ body }</p>
+                      <a className='upsell-anchor-text' href="">{ anchor }</a>
+                  </div>
+              ))
+          }
+        </div>
       </div>
     </div>
   );
--- src/components/section/section-container.js
+++ src/components/section/section-container.js
@@ -1,13 +1,20 @@
 import React from 'react';
 import copy from './data'
 
+function isEven(value) {
+	if (value%2 == 0)
+		return true;
+	else
+		return false;
+}
+
 export default function Section() {
   return (
     <div className='section'>
         {
-            copy.map(({title, body, image}) => (
-                <div className='section-main'>
-                    <div>
+            copy.map(({title, body, image}, index) => (
+                <div key={`${title}-${index}`} className={['section-main', isEven(index) ? '' : 'section-reverse'].join(' ')} >
+                    <div className='section-copy'>
                         <h1 className='section-title'>{title}</h1>
                         <p className='section-subtitle'>{body}</p>
                     </div>
