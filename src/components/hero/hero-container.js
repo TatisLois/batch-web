201ef4d5dd0d3b7b4438b394436370258cbe4014
Lois Tatis <ltatis1992@gmail.com>
2019-08-24 01:56:02 -0400

    layout updates

--- src/components/hero/hero-container.js
+++ src/components/hero/hero-container.js
@@ -1,11 +1,27 @@
-import React from 'react';
+import React, { useEffect } from 'react';
+import Typed from 'typed.js';
 import Form from './form';
 const heroURL = require('../../assets/images/hero.png')
 const heroSubURL = require('../../assets/images/hero-sub.png')
 const topRightCornerURL = require('../../assets/images/top-right.png')
 const logoURL = require('../../assets/images/batch.png');
 
+
+const options = {
+  strings: ["friends", "family", "co-workers"],
+  typeSpeed: 50,
+  backSpeed: 50,
+  startDelay: 1000,
+  showCursor: false
+}
+
+
 export default function Hero() {
+
+  useEffect(() => {
+    new Typed(".typed", options);
+  }, [])
+
   return (
     <div className='hero-blob'
       style={{
@@ -16,7 +32,7 @@ export default function Hero() {
       </div>
       <div className='hero'>
         <div className='hero-left'>
-          <h1 className='hero-title'>The best way to order delivery with friends </h1>
+          <h1 className='hero-title'>The best way to order delivery with <span className='typed'></span> </h1>
           <p className='hero-subtitle'>Batch is a food delivery app focused on what really matters. People. Whether you are in charge of getting everyones' order at work or having a get together with friends and family, we're here to make ordering easier and more fun! </p>
           <Form />
           <img className='hero-bottom-img' src={heroSubURL} />
